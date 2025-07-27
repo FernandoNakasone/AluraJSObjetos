7659e60701af224153812b27c3cf4d5af1f8e4c2
FernandoNakasone <fernandonakasone040@gmail.com>
2025-07-27 19:27:45 -0300

    introdução a objeto em js

---
+++ AluraJSObjetos/aulas/aula_1/objeto.js
@@ -0,0 +1,10 @@
+const listaCpf = ['1242353','1453467'];
+
+const infosPessoa = ['nome', 'josé Silva', 'idade', 32];
+
+const objPessoa = {
+    nome: 'José Silva',
+    idade: 32,
+    cpf: '123456789',
+    turma: 'JavaScript'
+}
