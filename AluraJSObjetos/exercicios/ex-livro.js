1b46a0d870f26d58b21d62fd5769eada9f97af22
FernandoNakasone <fernandonakasone040@gmail.com>
2025-07-31 22:46:25 -0300

    exercicio livro 2 com idade do livro

--- AluraJSObjetos/exercicios/ex-livro.js
+++ AluraJSObjetos/exercicios/ex-livro.js
@@ -5,4 +5,15 @@ const livro = {
     generos: ['Novela', 'Literatura infantil', 'Fábula', 'Ficção especulativa', 'Fantasia científica']
 }
 
-console.log(livro);
+console.log(`Nome:${livro.titulo} Autor:${livro.autor} Ano de lançamento:${livro.anoLancamento} Generos:${livro.generos}`);
+const anoAtual = new Date().getFullYear();
+
+const livro2 = {
+    titulo: 'O Pequeno Príncipe',
+    autor: 'Antoine de Saint-Exupéry',
+    anoLancamento: 1943,
+    generos: ['Novela', 'Literatura infantil', 'Fábula', 'Ficção especulativa', 'Fantasia científica'],
+    idade: anoAtual - livro.anoLancamento
+}
+
+console.log(`Nome:${livro2.titulo} Autor:${livro2.autor} Ano de lançamento:${livro2.anoLancamento} Generos:${livro2.generos} Idade:${livro2.idade}`);
