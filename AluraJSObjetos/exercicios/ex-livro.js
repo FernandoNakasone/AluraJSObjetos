3ade5370aa5276cd8d6e70f2bfe681a350b77d97
FernandoNakasone <fernandonakasone040@gmail.com>
2025-07-31 22:32:44 -0300

    exercicio objeto livro

---
+++ AluraJSObjetos/exercicios/ex-livro.js
@@ -0,0 +1,8 @@
+const livro = {
+    titulo: 'O Pequeno Príncipe',
+    autor: 'Antoine de Saint-Exupéry',
+    anoLancamento: 1943,
+    generos: ['Novela', 'Literatura infantil', 'Fábula', 'Ficção especulativa', 'Fantasia científica']
+}
+
+console.log(livro);
