fb4cea3d4ad62c167bcfb76c333d193d1c48a6ca
FernandoNakasone <fernandonakasone040@gmail.com>
2025-08-07 23:37:02 -0300

    Array no objeto, inicio da aula 2

---
+++ AluraJSObjetos/aulas/aula_2/dados-valores.js
@@ -0,0 +1,12 @@
+const estudante = {
+    nome: 'José Silva',
+    idade: 32,
+    cpf: '123456789',
+    turma: 'JavaScript',
+    bolsista: true,
+    telefone:['551199999999','551198879952']
+}
+
+console.log(estudante.telefone[0]);
+
+
